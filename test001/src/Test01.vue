680e2b916708f088c6154e3995ef5722b0cee816
Ben Chen <idben.chen@gmail.com>
2026-04-13 10:51:09 +0800

    Test01

---
+++ test001/src/Test01.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ref } from 'vue';
+
+const count = ref(0);
+const msg = ref("HelloWorld Vue");
+const name = ref('Vue');
+const isVisible = ref(true);
+const items = ref(['蘋果', '香蕉', '橘子']);
+
+count.value = 1;
+count.value++;
+items.value[count.value] = "西瓜"
+</script>
+<template>
+    <h1>{{msg}}</h1>
+    <h2>{{count}}</h2>
+    <h1>{{name}}</h1>
+    <h3>{{isVisible}}</h3>
+    <h1>{{ items[count] }}</h1>
+</template>
+<style scoped>
+h1{
+    font-weight: bold;
+    text-align: center;
+}
+</style>
