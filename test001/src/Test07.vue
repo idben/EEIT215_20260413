03df97ad6b708e18f0f7ce739255d60e1fe81e47
Ben Chen <idben.chen@gmail.com>
2026-04-13 15:31:45 +0800

    v-model

---
+++ test001/src/Test07.vue
@@ -0,0 +1,47 @@
+<script setup>
+import { ref } from 'vue';
+
+const userID = ref("")
+const userPWD = ref("")
+
+const handleSubmit = (e) => {
+    // e.preventDefault();
+    // console.log({ userID, userPWD }); // 簡寫
+    console.log({ userID: userID.value, userPWD: userPWD.value });
+}
+
+</script>
+<template>
+    <form @submit.prevent>
+        <div class="row">
+            <label for="userID">ID</label>
+            <input type="text" name="userID" id="userID" placeholder="輸入使用者帳號" v-model="userID"
+                @keydown.shift.enter.prevent="handleSubmit">
+        </div>
+
+        <div class="row">
+            <label for="userPWD">password</label>
+            <input type="password" name="userPWD" id="userPWD" placeholder="輸入密碼" v-model="userPWD"
+                @keydown.shift.enter.prevent="handleSubmit">
+        </div>
+
+        <div class="ctrl">
+            <button @click="handleSubmit">送出</button>
+        </div>
+
+    </form>
+</template>
+<style scoped>
+.row {
+    display: flex;
+
+    input {
+        flex: 1;
+    }
+}
+
+.ctrl {
+    display: flex;
+    justify-content: flex-end;
+}
+</style>
--- test001/src/main.js
+++ test001/src/main.js
@@ -1,6 +1,6 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
-import App from './Test06.vue'
+import App from './Test07.vue'
 
 createApp(App).mount('#app')
