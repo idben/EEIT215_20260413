d61a15ec462a92c23fc00bae5fb1de9bf9033836
Ben Chen <idben.chen@gmail.com>
2026-04-13 11:53:39 +0800

    Test03 文字插值{{}}

---
+++ test001/src/Test03.vue
@@ -0,0 +1,13 @@
+<script setup>
+const message = 'Hello Vue!'
+const isActive = false
+</script>
+<template>
+    <div>
+        <p>訊息: {{message}}</p>
+        <p>計算: {{1+1}}</p>
+        <p>呼叫方法: {{message.toUpperCase()}}</p>
+        <p>三元運算: {{ isActive ? "啟用": "停用"}}</p>
+    </div>
+</template>
+<style scoped></style>
--- test001/src/main.js
+++ test001/src/main.js
@@ -1,6 +1,6 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
-import App from './Test02.vue'
+import App from './Test03.vue'
 
 createApp(App).mount('#app')
